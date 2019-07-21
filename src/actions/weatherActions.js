c73b56bb6b89edf37e68936283d99f6517763b80
MonikaAnkoliya <monika.ankoliya@gmail.com>
2019-07-21 18:27:44 +0530

    Remove:- removed uunecessary debugger

--- src/actions/weatherActions.js
+++ src/actions/weatherActions.js
@@ -1,10 +1,8 @@
 export const getWeatherData = () => dispatch => {
-    debugger
     return new Promise((resolve,reject)=>{
         fetch('http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40')
             .then(response =>  response.json())
             .then(resData => {
-                debugger
                 // ADD TEMPERATURE IN CELCIUS AND FAHRENHEIT
                 const list = resData.list.map((data)=>{
                     const temperature = {
